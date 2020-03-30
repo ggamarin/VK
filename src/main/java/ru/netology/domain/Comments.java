3132be592e36e25607c13a99091f7c92c3f5850e
Grigoriy Gamarin <gam110191@gmail.com>
2020-03-30 17:57:21 +0300

    initial commit

---
+++ src/main/java/ru/netology/domain/Comments.java
@@ -0,0 +1,8 @@
+package ru.netology.domain;
+
+public class Comments {
+    private String commentID;
+    private String answeredID;
+    private String likesCounter;
+    private boolean canPost;
+}
---
+++ src/main/java/ru/netology/domain/Coordinates.java
@@ -0,0 +1,7 @@
+package ru.netology.domain;
+
+public class Coordinates {
+    private String type;
+    private String coordinates;
+
+}
