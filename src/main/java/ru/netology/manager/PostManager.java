f28e2df12e7861fc3a38bf4c0e907a954b1021c2
Grigoriy Gamarin <gam110191@gmail.com>
2020-03-30 20:54:06 +0300

    PostManager added

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(String[] args) {
+        return posts;
+    }
+
+    public void delete(int postId) {
+    }
+}
