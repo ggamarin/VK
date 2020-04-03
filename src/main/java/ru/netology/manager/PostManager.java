26452b286b4afbc3ffd136b7e8536fb750d1a415
Grigoriy Gamarin <gam110191@gmail.com>
2020-04-03 12:07:18 +0300

    added parameters

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -5,10 +5,10 @@ import ru.netology.domain.Post;
 public class PostManager {
     private Post[] posts;
 
-    public Post[] search(String[] args) {
+    public Post[] search(int ownerId, String domain, String query, boolean ownerOnly, int count,int offset) {
         return posts;
     }
 
-    public void delete(int postId) {
+    public void delete(int postId, int ownerId) {
     }
 }
